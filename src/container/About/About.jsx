8f84f9eace8e9c4f21343dd54154ad040b27d472
ArianAngoma <arian.angoma@tecsup.edu.pe>
2022-05-08 10:43:14 -0500

    About

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,33 +1,19 @@
 import {useState, useEffect} from 'react';
 import {motion} from 'framer-motion';
 
-import {images} from '../../constants';
+import {urlFor, client} from '../../client';
 import './About.scss';
 
-const abouts = [
-  {
-    title: 'Backend Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about02,
-  },
-  {
-    title: 'Frontend Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about03,
-  },
-  {
-    title: 'MERN Stack',
-    description: 'I am a good web developer',
-    imgUrl: images.about04,
-  },
-  {
-    title: 'Web Development',
-    description: 'I am a good web developer',
-    imgUrl: images.about01,
-  },
-];
-
 export const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => setAbouts(data));
+  }, []);
+
+
   return (
     <>
       <h2 className="head-text">
@@ -48,7 +34,10 @@ export const About = () => {
               className="app__profile-item"
               key={about.title + index}
             >
-              <img src={about.imgUrl} alt={about.title}/>
+              <img
+                src={urlFor(about.imgUrl)}
+                alt={about.title}
+              />
 
               <h2
                 className="bold-text"
